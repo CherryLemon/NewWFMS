2a9f702ee3d61f90cf07cacba8359e009420361b
CherryLemon <lemon_cn@sjtuer.net>
2015-04-29 00:39:38 +0800

    Updated file_uploader.ts

--- public/javascripts/file_uploader.ts
+++ public/javascripts/file_uploader.ts
@@ -1,19 +1,36 @@
-﻿class FileUploader
+﻿/// <reference path="../../typings/jquery/jquery.d.ts" />
+
+class FileUploader
 {
-    private fileQueue: FileList;
+    private fileQueue: Array<File>;
     private url: string;
     private progressControlContainer: JQuery;
+    //private isExecuting: boolean;
+
     constructor(url)
     {
         this.progressControl = "";
-        this.url = "";
+        this.url = url;
+        this.fileQueue = [];
+        setTimeout(this.queueDealerAsync, 1000);
     }
-    queueFiles(files)
+    public queueFiles(files)
     {
         if (files.length > 0)
         {
-
+            for(var i = 0; i < files.length; i++)
+                this.fileQueue.push(files[i]);
         }
+    }
+    private queueDealerAsync()
+    {
+        this.fileQueue.forEach(
+            function(e)
+            {
+                console.log(e);
+            }
+        );
+        setTimeout(this.queueDealerAsync, 1000);
     }
     private uploadFile(file: File)
     {
@@ -26,18 +43,18 @@ var uploader: FileUploader;
 
 $(document).ready(function () {
     var sel = $(".file-drag-drop");
-    sel.bind('dragover', function (e) {
-        console.log(e);
-    });
+   // sel.bind('dragover', function (e) {
+   //     console.log(e);
+   // });
     $(document).on('dragenter', function (e) { e.stopPropagation(); e.preventDefault(); });
     $(document).on('dragover', function (e) { e.stopPropagation(); e.preventDefault(); });
     $(document).on('drop', function (e) { e.stopPropagation(); e.preventDefault(); });
-
+    uploader = new FileUploader('http://localhost:3000/upload');
     sel.on('drop', function (e) {
         e.stopPropagation();
         e.preventDefault();
         var files = e.originalEvent.dataTransfer.files;
-        FileUploader.queueFiles(files);
+        uploader.queueFiles(files);
         return false;
     });
 
